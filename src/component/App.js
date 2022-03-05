812e79dfea29d1889c4b59a0875814495986c283
heechang2 <heechang2@heechang.com>
2022-03-05 23:31:28 +0900

    test

---
+++ src/component/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import axios from 'axios';
+
+export default class App extends React.Component {
+
+    componentDidMount(){
+        // 기본 api url 주소
+const URL_API = "http://localhost:8080/";
+
+// hello api url 주소
+const URL_HELLO = `${URL_API}`;
+
+        const url = "/";
+        axios.get(URL_HELLO)
+        .then(function(response) {
+            console.log(response.data);
+            console.log("성공");
+        })
+        .catch(function(error) {
+            console.log("실패");
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h1>My React App</h1>
+            </div>
+        )
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// react_start/webpack.config.js
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'index_bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
